15b33a4f68ec7ffb6ec86e4be1d2ad085675840b
kojima yosuke <yabbee0828@gmail.com>
2021-08-08 00:37:00 +0900

    add simple pq

--- data-structures/graph/dijkstra.js
+++ data-structures/graph/dijkstra.js
@@ -1,6 +1,28 @@
-// graphファイルでやっていた通り、基本はundirectedなgraphで考える。そこにweighの情報をのせていくこととする。
+// graphファイルでやっていた通り、基本はundirectedなgraphで考える。そこにweightの情報をのせていくこととする。
 class WeightedGraph {
   constructor() {
     this.adjacentList = {};
   }
+
+  addVertex(vertex) {
+    if (!this.adjacentList[vertex]) this.adjacentList[vertex] = [];
+    return this;
+  }
+
+  addEdge(vertex1, vertex2, weight) {
+    this.adjacentList[vertex1].push({ vertex: vertex2, weight: weight });
+    this.adjacentList[vertex2].push({ vertex: vertex1, weight: weight });
+    return this;
+  }
 }
+
+const wg = new WeightedGraph();
+wg.addVertex('A');
+wg.addVertex('B');
+wg.addVertex('C');
+wg.addEdge('A', 'B', 20);
+wg.addEdge('B', 'C', 10);
+wg.addEdge('A', 'C', 15);
+
+// console.dirが一番いいかも。JSONやらnode module使うのはめんどいや。
+console.dir(wg, { depth: null });
--- data-structures/tree/priorityQueue.js
+++ data-structures/tree/priorityQueue.js
@@ -82,3 +82,20 @@ pq.enqueue('analyst', 16);
 pq.enqueue('data scientist', 16);
 // pq.dequeue();
 console.log(pq);
+
+// simple priority queue
+class PriorityQueue {
+  constructor() {
+    this.values = [];
+  }
+  enqueue(val, priority) {
+    this.values.push({ val, priority });
+    this.sort();
+  }
+  dequeue() {
+    return this.values.shift();
+  }
+  sort() {
+    this.values.sort((a, b) => a.priority - b.priority);
+  }
+}
