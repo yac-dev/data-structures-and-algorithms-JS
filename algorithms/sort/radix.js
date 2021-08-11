e30a100fa54faa5671c3a4cf643cd3071ab0c415
kojima yosuke <yabbee0828@gmail.com>
2021-08-12 02:45:24 +0900

    Modify radix

--- algorithms/sort/radix.js
+++ algorithms/sort/radix.js
@@ -21,12 +21,14 @@ function mostDigits(nums) {
 function radixSort(nums) {
   let maxDigitCount = mostDigits(nums);
   for (let k = 0; k < maxDigitCount; k++) {
+    // let digitBuckets = new Array(10).fill([]); これ効かないね。。。Array.fromなんてstatic method、今まで使ったことないね。初めてだ。
     let digitBuckets = Array.from({ length: 10 }, () => []);
+    console.log(digitBuckets); // これ自体は[]の中に、[]のsub-arrayを0 ~ 9のindexに埋めている。
     for (let i = 0; i < nums.length; i++) {
-      let digit = getDigit(nums[i], k);
+      let digit = getDigit(nums[i], k); // 一つ一つの要素に関して、getDigitを加えている。
       digitBuckets[digit].push(nums[i]);
     }
-    nums = [].concat(digitBuckets.flat()); // ここが複雑だな。あまりこういう書き方をしたことがない。
+    nums = digitBuckets.flat(); // ここが複雑だな。あまりこういう書き方をしたことがない。
   } // digitBuckets自体は、[[231, 43,21], , [9], ,[32, 43]] っていうarrayになっている。
   // これを、...でdestructureすると  [231,43,21], undefined, [9], undefined, [32, 43] となる。
   return nums;
